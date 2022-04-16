01a635a9be63ed0e976fade82c64c959c5ff6216
Eternalding <wenny0916.iie08g@nctu.edu.tw>
2022-04-16 16:57:03 +0800

    Build basic hierarchies

---
+++ labrador/utils/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+LOGGING_LEVEL = {
+    "CRITICAL": 50, "ERROR": 40, "WARNING": 30,
+    "INFO"    : 20, "DEBUG": 10, "NOTSET" : 0
+}
+
+def init_logger(level:str="INFO"):
+    # Configurate logger
+    logging_level = LOGGING_LEVEL[level]
+    format = "['%(asctime)s %(filename)s:%(lineno)s - %(funcName)10s() ] %(message)s"
+    logging.basicConfig(format=format, level=logging_level, datefmt='%Y/%m/%d %I:%M:%S %p')
+
+    # Get logger
+    logger = logging.getLogger(__name__)
+    logger.info("Logger initialized.")
+
+
